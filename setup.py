daa0cefc98063391b3b12391f52254d428c1f667
Ash Wilson <ash.d.wilson@gmail.com>
2017-01-22 15:40:46 -0800

    chg: dev: Added setup.py file !minor

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import re
+from setuptools import setup
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name), 'r') as f:
+        filestring = f.read()
+    return filestring
+
+def get_version():
+    raw_init_file = read("opencellid/__init__.py")
+    rx_compiled = re.compile(r"\s*__version__\s*=\s*\"(\S+)\"")
+    ver = rx_compiled.search(raw_init_file).group(1)
+    return ver
+
+setup(name = "opencellid",
+      version = get_version(),
+      author = "Ash Wilson",
+      author_email = "ash.d.wilson@gmail.com",
+      description = "A python wrapper for the OpenCellID DB",
+      license = "BSD",
+      keywords = "opencellid",
+      url = "https://github.com/ashmastaflash/opencellid-wrapper",
+      packages = ["opencellid"],
+      install_requires = ["requests"],
+      long_description = read("README.rst"))
