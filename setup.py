01159e4e3f292db9329e3ad09a069c1a78b4ae9b
Ash Wilson <ash.d.wilson@gmail.com>
2017-06-12 20:28:57 -0700

    chg: dev: PEP8 for setup.py !minor

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 import re
 from setuptools import setup
 
+
 def read(file_name):
     with open(os.path.join(os.path.dirname(__file__), file_name), 'r') as f:
         filestring = f.read()
     return filestring
+
 
 def get_version():
     raw_init_file = read("opencellid/__init__.py")
@@ -13,19 +15,20 @@ def get_version():
     ver = rx_compiled.search(raw_init_file).group(1)
     return ver
 
+
 def build_long_desc():
     return "\n".join([read(f) for f in ["README.rst", "CHANGELOG.rst"]])
 
 
-setup(name = "opencellid",
-      version = get_version(),
-      author = "Ash Wilson",
-      author_email = "ash.d.wilson@gmail.com",
-      description = "A python wrapper for the OpenCellID DB",
-      license = "BSD",
-      keywords = "opencellid",
-      url = "https://github.com/ashmastaflash/opencellid-wrapper",
-      packages = ["opencellid"],
-      install_requires = ["requests"],
-      long_description = build_long_desc(),
-      classifiers = ['Programming Language :: Python :: 2.7'])
+setup(name="opencellid",
+      version=get_version(),
+      author="Ash Wilson",
+      author_email="ash.d.wilson@gmail.com",
+      description="A python wrapper for the OpenCellID DB",
+      license="BSD",
+      keywords="opencellid",
+      url="https://github.com/ashmastaflash/opencellid-wrapper",
+      packages=["opencellid"],
+      install_requires=["requests"],
+      long_description=build_long_desc(),
+      classifiers=['Programming Language :: Python :: 2.7'])
